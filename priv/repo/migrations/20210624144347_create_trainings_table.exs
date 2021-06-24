210c744d856d52d49de0f2813530f5daf80e06c6
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-06-24 11:55:29 -0300

    fix trainings table name

--- priv/repo/migrations/20210624144347_create_trainings_table.exs
+++ priv/repo/migrations/20210624144347_create_trainings_table.exs
@@ -2,7 +2,7 @@ defmodule Wabanex.Repo.Migrations.CreateTrainingsTable do
   use Ecto.Migration
 
   def change do
-    create table(:training) do
+    create table(:trainings) do
       add :start_date, :date
       add :end_date, :date
       add :user_id, references(:users)
